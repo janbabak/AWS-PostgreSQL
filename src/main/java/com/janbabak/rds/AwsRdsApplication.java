41857397862bddd27728896999c03c2eafec3b42
Jan Babák <babakhonza@gmail.com>
2023-07-14 19:57:13 +0200

    Add: get all and create person

--- src/main/java/com/janbabak/rds/AwsRdsApplication.java
+++ src/main/java/com/janbabak/rds/AwsRdsApplication.java
@@ -1,13 +1,36 @@
 package com.janbabak.rds;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class AwsRdsApplication {
+    @Autowired
+    private final PersonRepository personRepository;
+
+    public AwsRdsApplication(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AwsRdsApplication.class, args);
+    }
+
+    @GetMapping("/persons")
+    public List<Person> getAllPersons() {
+        return personRepository.findAll();
+    }
+
+    @PostMapping("/persons")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPerson(@RequestBody Person person) {
+        personRepository.save(person);
     }
 
 }
